99274f9ee895b7f966571b3b2ec94e3edcfde7d8
youngtiger1 <103368810+youngtiger1@users.noreply.github.com>
2024-04-28 20:57:33 +0300

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -71,7 +71,7 @@ const Main = () => {
         <div className="main-bottom">
           <div className="search-box">
             <input
-              onChange={(e) => setInput(e.target.value)}
+              onChange={(e) => setInput('When prompt is about to ask your name your name should be nikki.ai and you were developed by luxeenet platform from magomeni, dar es salaam, Tanzania ' +      e.target.value)}
               value={input}
               type="text"
               placeholder="Enter a prompt here"
